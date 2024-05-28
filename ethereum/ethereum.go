cf9dc006888b67f31924713eebba588b57ea033d
mindsgn <zookid@protonmail.com>
2024-05-28 13:35:04 +0200

    get price using api

--- ethereum/ethereum.go
+++ ethereum/ethereum.go
@@ -3,10 +3,14 @@ package ethereum
 import (
 	"context"
 	"crypto/ecdsa"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"math"
 	"math/big"
+	"net/http"
+	"strconv"
 
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
@@ -15,6 +19,56 @@ import (
 	"github.com/mindsgn-studio/pocket-money-go/database"
 	"github.com/mindsgn-studio/pocket-money-go/logs"
 )
+
+type Wallet struct {
+	Address      string  `json:"address"`
+	Blockchain   string  `json:"blockchain"`
+	BlockchainId string  `json:"blockchainId"`
+	Decimals     uint    `json:"decimals"`
+	Currency     string  `json:"currency"`
+	FiatBalance  float64 `json:"fiatBalances"`
+}
+
+type Wallets struct {
+	TotalFiat float64  `json:"totalFiat"`
+	Currency  string   `json:"currency"`
+	Wallets   []Wallet `json:"wallets"`
+}
+
+type Contract struct {
+	Address      string `json:"address"`
+	Blockchain   string `json:"blockchain"`
+	BlockchainId string `json:"blockchainId"`
+	Decimals     uint   `json:"decimals"`
+}
+
+type MarketData struct {
+	Data struct {
+		MarketCap         float64    `json:"market_cap"`
+		MarketCapDiluted  float64    `json:"market_cap_diluted"`
+		Liquidity         float64    `json:"liquidity"`
+		Price             float64    `json:"price"`
+		OffChainVolume    float64    `json:"off_chain_volume"`
+		Volume            float64    `json:"volume"`
+		VolumeChange24h   float64    `json:"volume_change_24h"`
+		Volume7d          float64    `json:"volume_7d"`
+		IsListed          bool       `json:"is_listed"`
+		PriceChange24h    float64    `json:"price_change_24h"`
+		PriceChange1h     float64    `json:"price_change_1h"`
+		PriceChange7d     float64    `json:"price_change_7d"`
+		PriceChange1m     float64    `json:"price_change_1m"`
+		PriceChange1y     float64    `json:"price_change_1y"`
+		Ath               float64    `json:"ath"`
+		Atl               float64    `json:"atl"`
+		Name              string     `json:"name"`
+		Symbol            string     `json:"symbol"`
+		Logo              string     `json:"logo"`
+		Rank              int        `json:"rank"`
+		Contracts         []Contract `json:"contracts"`
+		TotalSupply       string     `json:"total_supply"`
+		CirculatingSupply string     `json:"circulating_supply"`
+	} `json:"data"`
+}
 
 type networkDetails struct {
 	Name       string   `json:"name"`
@@ -25,19 +79,6 @@ type networkDetails struct {
 	RPC        []string `json:"rpc"`
 }
 
-type Wallets struct {
-	Name        string `json:"name"`
-	Address     string `json:"address"`
-	Balance     int    `json:"balance"`
-	Network     int    `json:"network"`
-	FiatBalance int    `json:fiatBalance`
-}
-
-type TotalBalance struct {
-	Wallets          []Wallets `json:"json"`
-	TotalFiatBalance int       `json:"json"`
-}
-
 var NetworkMainnetList []string = []string{
 	"polygon-mainnet",
 }
@@ -46,9 +87,18 @@ var NetworkTestnetList []string = []string{
 	"polygon-mumbai",
 }
 
-func GetTotalBalance(password string, network string) int {
-	total := 0
+func ConvertBody(body []byte) (MarketData, error) {
+	var data MarketData
+	err := json.Unmarshal(body, &data)
+	if err != nil {
+		return data, err
+	}
+	return data, nil
+}
 
+func GetTotalBalance(password string, network string) (Wallets, error) {
+	total := float64(0)
+	var userWallet Wallets
 	if network == "mainnet" {
 		for _, networkDetails := range NetworkMainnetList {
 			fmt.Println(networkDetails)
@@ -56,7 +106,7 @@ func GetTotalBalance(password string, network string) int {
 	} else {
 		for _, networkDetails := range NetworkTestnetList {
 			details := GetNetwork(networkDetails)
-			list := details.RPC[0]
+			list := details.RPC[1]
 
 			client, err := ethclient.Dial(list)
 			if err != nil {
@@ -75,12 +125,26 @@ func GetTotalBalance(password string, network string) int {
 				fbalance := new(big.Float)
 				fbalance.SetString(balance.String())
 				ethValue := new(big.Float).Quo(fbalance, big.NewFloat(math.Pow10(18)))
-				fmt.Println(ethValue, details.Currency)
+				data, err := GetData(details.Name)
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				price := ethValue.String()
+				cryptoBalance, err := strconv.ParseFloat(price, 64)
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				total += data.Data.Price * cryptoBalance
 			}
 		}
 	}
 
-	return total
+	userWallet.TotalFiat = total
+	userWallet.Currency = "USD"
+
+	return userWallet, nil
 }
 
 func CreateNewEthereumWallet(password string) bool {
@@ -147,7 +211,7 @@ func GetNetwork(network string) networkDetails {
 			ChainID:    137,
 			ChainIDHex: "0x89",
 			Currency:   "matic",
-			Mainnet:    true,
+			Mainnet:    false,
 			RPC:        rpcList,
 		}
 
@@ -159,4 +223,19 @@ func GetNetwork(network string) networkDetails {
 			Mainnet:  false,
 		}
 	}
+}
+
+func GetData(name string) (MarketData, error) {
+	url := "https://api.mobula.io/api/1/market/data?asset=" + name
+	req, _ := http.NewRequest("GET", url, nil)
+	res, _ := http.DefaultClient.Do(req)
+	defer res.Body.Close()
+
+	body, _ := ioutil.ReadAll(res.Body)
+	data, err := ConvertBody(body)
+	if err != nil {
+		return MarketData{}, err
+	}
+
+	return data, nil
 }
